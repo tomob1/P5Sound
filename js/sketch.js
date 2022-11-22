fc535035beb28e3c3ce89c5462e4f2cb584a23bf
Tom O'Brien <116882874+tomob1@users.noreply.github.com>
2022-11-22 10:56:52 +0000

    Add files via upload
    Added Button + Music Assests

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,52 @@
-function setup() {
-    var canvas = createCanvas(800, 800);
-    canvas.parent("p5container");
+var mySong, amplitude;
+var mousePressed
+
+
+function preload() { //preload starts before setup, prevent buffering or errors from large files like media files
+    soundFormats('mp3');
+    mySong = loadSound('assets/massiveAttackAngel.mp3');
 }
 
-function draw() {
-    background(220);
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+function setup() { //setup runs once at the start
+    let canvas = createCanvas(window.innerWidth, window.innerHeight);
+   canvas.parent("p5container");
+   
+
+    mySong.play();
+    background(1);
+    noStroke();
+    amplitude = new p5.Amplitude();
+    rectMode(CENTER);
+    console.log(amplitude);
+
+
+play = false;
+var button = createButton("Play");
+button.center();
+button.addClass('btn')
+
+button.mousePressed(function() {
+play = true;
+mySong.play();
+button.hide();
+});
+
+
 }
+
+function draw() {  //draw runs 60 frames a second
+    
+    var level = amplitude.getLevel();
+    var vol = map(level, 0, 1, 0, 1000);
+    var col = map(level, 0, 1, 0, 255);
+
+    background(col);
+
+    fill(col, 255, 255);
+    ellipse(width / 2, height / 2, vol);
+    
+    fill(255, 255, col);
+    ellipse(width / 2, height / 2, vol/2);
+}
+

af2e553504c0779528e70b37032c77250cbfe19a
Tom O'Brien <116882874+tomob1@users.noreply.github.com>
2022-11-25 12:59:32 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,52 +1,39 @@
-var mySong, amplitude;
-var mousePressed
+var myArray = ["five", "four", "three", "two", "one"];
+function setup() {
+    var canvas = createCanvas(800, 800);
+    canvas.parent("p5container");
+}
+    // for(var i = 0; i < 10; i++){
+    //     console.log('I want' + i + 'Ice creams')
 
+    // }
 
-function preload() { //preload starts before setup, prevent buffering or errors from large files like media files
-    soundFormats('mp3');
-    mySong = loadSound('assets/massiveAttackAngel.mp3');
+//  for (var i = 0; i < myArray.length; i++){
+//      console.log(myArray[i]);
+// }
+
+for(var words in myArray){
+    console.log(myArray[words]);
 }
 
+function draw() {
+    background(220);
 
-function setup() { //setup runs once at the start
-    let canvas = createCanvas(window.innerWidth, window.innerHeight);
-   canvas.parent("p5container");
-   
-
-    mySong.play();
-    background(1);
-    noStroke();
-    amplitude = new p5.Amplitude();
-    rectMode(CENTER);
-    console.log(amplitude);
-
-
-play = false;
-var button = createButton("Play");
-button.center();
-button.addClass('btn')
-
-button.mousePressed(function() {
-play = true;
-mySong.play();
-button.hide();
-});
-
-
+    stroke(255);
 }
 
-function draw() {  //draw runs 60 frames a second
-    
-    var level = amplitude.getLevel();
-    var vol = map(level, 0, 1, 0, 1000);
-    var col = map(level, 0, 1, 0, 255);
+    // for (var lineX = 75; lineX <= width; lineX += 75) {
+    //     line(lineX, 0, lineX, height);
+    // }
 
-    background(col);
+    // for(var x = 0; x <=80; x++){
+    //     circle(x * 10, height/2, 10)
+    // }
 
-    fill(col, 255, 255);
-    ellipse(width / 2, height / 2, vol);
-    
-    fill(255, 255, col);
-    ellipse(width / 2, height / 2, vol/2);
-}
-
+//         for(var y = 0; y <= 80; y++){
+//             for(var x = 0; x <=80; x++) {
+//                 fill(x * y, x * 10, y);
+//                 circle(x * 10, y * 10, 5)
+//             }
+//         }
+// }
